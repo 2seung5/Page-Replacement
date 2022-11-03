0e10965967206451904d9d64e374562c6a3bac34
Seung Ho <dltmdgh0904@naver.com>
2022-11-04 08:21:05 +0900

    second commit

--- Page Replacement Simulator/simulator.c
+++ Page Replacement Simulator/simulator.c
@@ -1,54 +1,97 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int fn;
+int string[500];
+
+void interface()
+{
+    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
+    int fn;
+    scanf("%d",&fn);
+    if ( fn<3 || fn>10)
+        printf("다시 입력하세요 (3~10)\n");
+    else{
+        printf("데이터 입력 방식을 선택하시오(1,2)\n");
+        int ch;
+        scanf("%d",&ch);
+        if (ch==1)
+        {
+            getData1();
+        }
+        else if (ch==2)
+        {
+            getData2();
+        }
+        else
+            printf("다시 입력하세요 1,2\n");
+    }
+}
+
+void getData1()  // 프로그램 내부에서 입력 받는 방식
+{
+    int in[500];
+
+    for (int i = 0; i < 500; i++)
+    {
+        in[i]=rand()%30;
+        string[i] = in[i];
+    }
+    
+}
+
+void getData2() //파일로 저장하여 이를 오픈하여 입력받는 방식??????????????
+{   
+    int in[500];
+    FILE *fp =fopen("string.txt","w");
+    for (int i = 0; i < 500; i++)
+    {
+        in[i]=rand()%30;
+        fprintf(fp,"%d\n",in[i]);
+    }
+    fgets(string,sizeof(string), fp);  //이게 맞나?
+    
+    fclose(fp);
+
+    
+
+   
+}
 
 
 void optimal()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+    
 }
 
 void fifo()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+   
 }
 
 void lifo()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+
 }
 
 void lru()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+    
 }
-
 void lfu()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+   
 }
 
 void sc()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+  
 
 }
 
 void esc()
 {
-    printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int n;
-    scanf("%d",&n);
+    
 }
 
 int main()
