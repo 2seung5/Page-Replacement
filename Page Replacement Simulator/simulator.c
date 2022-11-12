cfd5d2c8306b7799df70414d8bb67bc1ef4b6e91
Seung Ho <dltmdgh0904@naver.com>
2022-11-13 03:43:05 +0900

    3번째

--- Page Replacement Simulator/simulator.c
+++ Page Replacement Simulator/simulator.c
@@ -1,13 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fn;
-int string[500];
+int fn;              //페이지 프레임 개수
+int stream[10];     //참조 페이지 스트링
+int hit;             //hit 변수 0 or 1
+int pagefaultcnt;    //페이지 폴트 카운터
+int p[100];
+
+void getData1()  // 프로그램 내부에서 입력 받는 방식
+{
+    int in[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        in[i]=rand()%30;
+        stream[i] = in[i];
+    }
+    
+}
+
+void getData2() //파일로 저장하여 이를 오픈하여 입력받는 방식??????????????
+{   
+    int in[10];
+    FILE *fp =fopen("string.txt","w");
+    for (int i = 0; i < 10; i++)
+    {
+        in[i]=rand()%30;
+        fprintf(fp,"%d\n",in[i]);
+    }
+    fgets(stream,sizeof(stream), fp);  //이게 맞나?
+    
+    fclose(fp);
+   
+}
+
+void initialize()          // 초기화 해주는 함수?
+{
+    pagefaultcnt=0;
+    for(int i=0; i<fn; i++)
+        p[i]=9999;
+}
+
+int isHit(int data)          //hit 확인하는 함수
+{
+    hit=0;
+    for(int j=0; j<fn; j++)
+    {
+        if(p[j]==data)
+        {
+            hit=1;
+            break;
+        }
+
+    }
+    return hit;
+}
+
+void pageFaultCnt()          //페이지 폴트 개수 나타내주는 함수
+{
+    printf("\nnumber of page faults: %d",pagefaultcnt);
+}
 
 void interface()
 {
     printf("페이지 프레임의 개수를 입력하시오.(3~10)\n");
-    int fn;
+    // int fn;
+    
     scanf("%d",&fn);
     if ( fn<3 || fn>10)
         printf("다시 입력하세요 (3~10)\n");
@@ -28,70 +86,42 @@ void interface()
     }
 }
 
-void getData1()  // 프로그램 내부에서 입력 받는 방식
-{
-    int in[500];
-
-    for (int i = 0; i < 500; i++)
-    {
-        in[i]=rand()%30;
-        string[i] = in[i];
-    }
-    
-}
-
-void getData2() //파일로 저장하여 이를 오픈하여 입력받는 방식??????????????
-{   
-    int in[500];
-    FILE *fp =fopen("string.txt","w");
-    for (int i = 0; i < 500; i++)
-    {
-        in[i]=rand()%30;
-        fprintf(fp,"%d\n",in[i]);
-    }
-    fgets(string,sizeof(string), fp);  //이게 맞나?
-    
-    fclose(fp);
-
-    
-
-   
-}
-
 
 void optimal()
 {
-    
+    interface();
 }
 
 void fifo()
 {
-   
+     interface();
+     
+     
 }
 
 void lifo()
 {
-
+     interface();
 }
 
 void lru()
 {
-    
+     interface();
 }
 void lfu()
 {
-   
+     interface();
 }
 
 void sc()
 {
-  
+     interface();
 
 }
 
 void esc()
 {
-    
+     interface();
 }
 
 int main()
@@ -100,7 +130,7 @@ int main()
     int ch;
     while(1){
         printf("Page Replacement 알고리즘 시뮬레이터를 선택하시오 (최대3개)\n");
-        printf("1.Optimal  2.FIFO  3.LIFO  4.LRU  5.LFU  6.SC  7.ESC\n");
+        printf("1.Optimal  2.FIFO  3.LIFO  4.LRU  5.LFU  6.SC  7.ESC 8.exit\n");
 
         scanf("%d",&ch);
         switch (ch)
@@ -129,6 +159,10 @@ int main()
         
         case 7 :
             esc();    
+            break;
+
+        case 8 :
+            return 0;
             break;
         
         default:
